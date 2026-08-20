212df2f92cfbb12bfbedbf7d4786298497567414
Koji Machida(Ejiri) <200968+eji@users.noreply.github.com>
2026-08-20 10:42:47 +0900

    Use Go 1.27.0 release toolchain

--- flake.nix
+++ flake.nix
@@ -17,24 +17,59 @@
       devShells = forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; };
+          goArchives = {
+            aarch64-darwin = {
+              name = "darwin-arm64";
+              hash = "sha256-kEk7O71eEPkdEhUxmL8ZlP11Y5m0/sk7SbDG4qze6z4=";
+            };
+            x86_64-darwin = {
+              name = "darwin-amd64";
+              hash = "sha256-0zFOJUluQ4HXGlxR0pB+evZV0Zn2eAtUnwFb2F/vSYY=";
+            };
+            aarch64-linux = {
+              name = "linux-arm64";
+              hash = "sha256-UXmNLELQ4cbtf9n0hyi0GTq6yeiq1tusL+lqgfWQm9o=";
+            };
+            x86_64-linux = {
+              name = "linux-amd64";
+              hash = "sha256-Z1wmxEnLsY/CS3RlDeHqu65uFvZDJv2FooP7O1goBoU=";
+            };
+          };
+          goArchive = goArchives.${system};
+          go_1_27 = pkgs.stdenvNoCC.mkDerivation {
+            pname = "go";
+            version = "1.27.0";
+            src = pkgs.fetchurl {
+              url = "https://go.dev/dl/go1.27.0.${goArchive.name}.tar.gz";
+              inherit (goArchive) hash;
+            };
+            sourceRoot = "go";
+            dontBuild = true;
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out"
+              cp -R . "$out"
+              runHook postInstall
+            '';
+          };
         in
         {
           default = pkgs.mkShell {
             packages = [
-              pkgs.go_1_27
+              go_1_27
               pkgs.gnumake
               pkgs.gitleaks
               pkgs.pre-commit
             ];
 
             shellHook = ''
-              expected="go version go1.27rc2"
+              expected="go version go1.27.0"
               actual="$(go version)"
               case "$actual" in
                 "$expected"*) ;;
                 *)
-                  echo "tiqq: expected Go 1.27 RC2, got: $actual" >&2
-                  return 1
+                  echo "tiqq: expected Go 1.27.0, got: $actual" >&2
+                  exit 1
                   ;;
               esac
 
